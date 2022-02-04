87f87750edd0c526d8b769b5c21c36525297e56a
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2022-02-03 23:24:41 -0400

    Serialize highest score

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainManager : MonoBehaviour
+public class MainManager : MonoBehaviour, ISaveable
 {
+    public static MainManager Instance { get; private set; }
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
 
+    [Header("Current Score")]
     public Text ScoreText;
+    [SerializeField] private Text NameText;
+    public SaveData currentScore { get; set; }
+
+    [Header("Highest Score")]
+    [SerializeField] private Text highestScoreText;
+    [SerializeField] private Text highestNameText;
+    public SaveData savedHighestScore { get; set; }
+
     public GameObject GameOverText;
     
     private bool m_Started = false;
-    private int m_Points;
     
     private bool m_GameOver = false;
 
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +44,7 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+
     }
 
     private void Update()
@@ -64,13 +73,63 @@ public class MainManager : MonoBehaviour
 
     void AddPoint(int point)
     {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        currentScore.score += point;
+        ScoreText.text = $"Score : {currentScore.score}";
     }
 
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+        checkHighestScore();
+    }
+
+    private void checkHighestScore()
+    {
+        if (currentScore.score > savedHighestScore.score)
+        {
+            SaveScore();
+        }
+    }
+
+    private void SaveScore()
+    {
+        // Update display information of highest score
+        highestScoreText.text = ScoreText.text;
+        highestNameText.text = NameText.text;
+        // Update saved highest score
+        savedHighestScore.score = currentScore.score;
+        savedHighestScore.name = currentScore.name;
+        IEnumerable<MainManager> a_saveables = new List<MainManager>() { this };
+        SaveDataManager.SaveJsonData(a_saveables);
+    }
+
+    void ISaveable.PopulateSaveData(SaveData a_SaveData)
+    {
+        a_SaveData.name = savedHighestScore.name;
+        a_SaveData.score = savedHighestScore.score;
+    }
+
+    void ISaveable.LoadFromSaveData(SaveData a_SaveData)
+    {
+        savedHighestScore.name = a_SaveData.name;
+        savedHighestScore.score = a_SaveData.score;
+    }
+
+    private void Awake()
+    {
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        currentScore = new SaveData(MenuUI.Instance.savedCurrentScore.name);
+        NameText.text = $"{NameText.text} {currentScore.name}";
+        ScoreText.text = $"{ScoreText.text} {currentScore.score}";
+
+        // Display information of highest score
+        savedHighestScore = MenuUI.Instance.savedHighestScore;
+        highestNameText.text = $"{highestNameText.text} {savedHighestScore.name}";
+        highestScoreText.text = $"{highestScoreText.text} {savedHighestScore.score}";
     }
 }
